35fae2ae19cbd782d5b94e983550986307b5782d
Marcsoussand <marc.soussand@gmail.com>
2026-02-15 10:28:08 +0200

    Move files to week 0

---
+++ DI-Bootcamp-Stage1/Week1/Day1/lesson1.py
@@ -0,0 +1,19 @@
+# print('Hello to Python')
+#basic math operations
+# print(2+2) # =4
+# print(10-3) #=7
+# print(20/4) #=5.0
+# print(10//3) #=3
+# print(10%3) #=1
+# print(2**8) #=256
+
+#Order of operations
+# print (5+3*2) #=11
+# print ((5+3)*2) #=16
+greeting = "Hello Class"
+print(type(greeting)) #=<class 'str'>
+print(type('hi'))
+print(type(123))
+print(type("123"))
+
+# String method
