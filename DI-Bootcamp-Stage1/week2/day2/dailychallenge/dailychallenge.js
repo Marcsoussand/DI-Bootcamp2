6090fc60bef2287e995b03e2963b7a127c733376
Marcsoussand <marc.soussand@gmail.com>
2026-02-23 23:49:07 +0200

    Daily Challenge Week 2 day 2

---
+++ DI-Bootcamp-Stage1/week2/day2/dailychallenge/dailychallenge.js
@@ -0,0 +1,19 @@
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturne', 'Uranus', 'Neptune']
+let divArray = [];
+let planetColors = ['brown', 'orange', 'blue', 'red', 'white','yellow', 'green','light blue']
+const style = document.getElementsByTagName('style')[0]
+console.log(style);
+
+
+for (let i in planets) {
+    divArray[i] = document.createElement('div');
+    divArray[i].classList.add('planet');
+    divArray[i].classList.add(planets[i]);
+    document.body.getElementsByClassName('listPlanets')[0].appendChild(divArray[i]);
+    console.log(`\n .${planets[i]} { background: ${planetColors[i]}}`);
+    
+    style.textContent += `\n.${planets[i]} { background: ${planetColors[i]}}`
+}
+console.log(divArray);
+
+
