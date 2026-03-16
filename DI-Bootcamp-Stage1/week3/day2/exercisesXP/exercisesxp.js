31e9731277a176f526413120c583b2e6d7bf854f
Marcsoussand <marc.soussand@gmail.com>
2026-03-16 22:15:38 +0200

    exercises XP week3 day 2

--- DI-Bootcamp-Stage1/week3/day2/exercisesXP/exercisesxp.js
+++ DI-Bootcamp-Stage1/week3/day2/exercisesXP/exercisesxp.js
@@ -25,3 +25,57 @@ function displayStudentInfo(objUser){
 }
 
 displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+//Exercise 3
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+const usersArray = Object.entries(users);
+console.log(usersArray);
+//2. Modify the outcome of part 1, by multipling the user’s ID by 2.
+let usersArray2 = []
+for (let [key, value] of Object.entries(users)) {
+  usersArray2.push([key, value*2]);
+}
+console.log(usersArray2);
+
+//Exercise4
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+
+//Output Expected = object
+
+//Exercise 5 
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+};
+
+  // 2 is the correct one
+class Labrador extends Dog {
+  constructor(name, size) {
+    super(name);
+    this.size = size;
+  }
+};
+
+//Exercise 6 
+
+// [2] === [2] 
+// {} === {}
+//False for both as they are reference types
+
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5};
+
+object1.number = 4; //4
+console.log(object3.number) //4
+console.log(object4.number) //5
