a181983dd4c8822f80792d0e2a7f7d8657098ecc
Marcsoussand <marc.soussand@gmail.com>
2026-04-19 12:57:35 +0300

    Daily Challenge W6D1

---
+++ DI-Bootcamp-Stage1/week6/day1/dailyChallenge.sql
@@ -0,0 +1,10 @@
+
+--Instructions 
+--1. Count how many actors are in the table.
+-- SELECT COUNT(1) from actors;
+-- 2. Try to add a new actor with some blank fields.
+-- INSERT into actors VALUES (3,'','Marc','1983-09-16',2);
+-- What do you think the outcome will be ?
+-- Just adding blank fields 
+
+SELECT * from actors;
