07f03645df5a4a4ca0980aba1ac7665a46ba1d9e
Marcsoussand <marc.soussand@gmail.com>
2026-04-23 22:59:43 +0300

    Update with DB

--- DI-Bootcamp-Stage1/week6/day4/ExercisesXP/blog-api/server/config/config.js
+++ DI-Bootcamp-Stage1/week6/day4/ExercisesXP/blog-api/server/config/config.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
 const PORT = process.env.PORT || 3000;
 
-module.exports = { PORT };
+const db = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = { PORT, db };
